74f0c93dbf94e8282bacae5861e47db72cc730dd
Alexandre Anastassov <a.anastassov@iaea.org>
2018-03-29 16:09:12 +0200

    add solution files

---
+++ src/CharacterApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Library;
+
+namespace CharacterApp
+{
+    class Program
+    {
+        private static readonly char[] Alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        static void Main(string[] args)
+        {
+            string phrase;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter a phrase");
+                phrase = Console.ReadLine();
+            } 
+            else 
+            {
+                if (!File.Exists(args[0])) {
+                    Console.WriteLine("Enter a valid file name");
+                    return;
+                }
+                phrase = File.ReadAllText(args[0]);
+            }
+
+            CharacterCount counter = new CharacterCount();
+
+            counter.ParseString(phrase);
+
+            foreach (var letter in Alphabet)
+            {
+                Console.WriteLine("{0} : {1}", letter, counter.GetCountForLetter(letter));
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            phrase = Console.ReadLine();
+        }
+    }
+}
---
+++ src/CharacterLib/CharacterCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class CharacterCount
+    {
+        readonly Dictionary<char, int> _counts = new Dictionary<char, int>();
+
+        readonly char[] _alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        public CharacterCount() {
+            foreach (var letter in _alphabet)
+            {
+                _counts.Add(letter, 0);
+            }
+        }
+
+        public void ParseString(string phrase) {
+            char[] charactersInPhrase = phrase.ToLower().ToCharArray();
+
+            foreach (var letter in charactersInPhrase)
+            {
+                if(Char.IsLetter(letter)) {
+                    _counts[letter] = _counts[letter] + 1;
+                }
+            }
+
+        }
+
+        public int GetCountForLetter(char letter) {
+            return _counts[letter];
+        }
+    }
+}
